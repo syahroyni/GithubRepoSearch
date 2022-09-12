25707cb28617129a49b0a48b79d1fee743092cc1
Muhammad Syah royni <syah.royni@dana.id>
2022-09-12 21:22:02 +0900

    Assign tableViewCell component to properties in RepositoryTableViewCell class

--- GithubRepositorySearch/RepositoryTableViewCell.swift
+++ GithubRepositorySearch/RepositoryTableViewCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class RepositoryTableViewCell: UITableViewCell {
 
+	@IBOutlet weak var repositoryNameLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var lastUpdatedLabel: UILabel!
+	@IBOutlet weak var watcherCountLabel: UILabel!
+	@IBOutlet weak var starCountLabel: UILabel!
+	
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
