bc1a1c5de6606c395b2ed8b612b5f93f791a3c9a
Muhammad Syah royni <syah.royni@dana.id>
2022-09-13 13:52:59 +0900

    Set RepositoryTableView data source

--- GithubRepositorySearch/RepositoryViewController.swift
+++ GithubRepositorySearch/RepositoryViewController.swift
@@ -17,8 +17,29 @@ class RepositoryViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		repositoriesTableView.dataSource = self
 	}
 
-
+}
+extension RepositoryViewController: UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		return viewModel.listRepositories.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		if let cell = tableView.dequeueReusableCell(withIdentifier: "repositoryCell") as? RepositoryTableViewCell {
+			
+			let repositoryModel = viewModel.listRepositories[indexPath.row]
+			cell.setRepositoryModel(repository: repositoryModel)
+			
+			return cell
+		}
+		
+		return UITableViewCell()
+	}
 }
 
