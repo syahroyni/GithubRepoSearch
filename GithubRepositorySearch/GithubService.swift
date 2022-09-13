8dcff683da6913bf92821229fe294b4475683681
Muhammad Syah royni <syah.royni@dana.id>
2022-09-13 11:54:04 +0900

    Fetch Github Search API with RESTService and parse the result to the model

--- GithubRepositorySearch/GithubService.swift
+++ GithubRepositorySearch/GithubService.swift
@@ -24,5 +24,37 @@ class GithubService: GithubFacade {
 	
 	func searchRepository(queryString: String, completion: @escaping (Result<SearchRepositoryResult, Error>) -> Void) {
 		
+		let urlString = "\(self.baseURL)search/repositories"
+		var urlComponents = URLComponents(string: urlString)
+		let queryItem = URLQueryItem(name: "q", value: queryString)
+		urlComponents?.queryItems = [queryItem]
+		
+		guard let url = urlComponents?.url else {
+			return
+		}
+		
+		restService.fetch(url: url, completion: { data, error in
+			
+			if let error = error {
+				
+				completion(.failure(error))
+			} else if let data = data {
+				
+				let decoder = JSONDecoder()
+				
+				if let searchResult = try? decoder.decode(SearchRepositoryResult.self, from: data) {
+					
+					completion(.success(searchResult))
+				} else {
+					let parseError = ResponseError(errorMessage: "Failed to parse the result", errorCode: 400)
+					
+					completion(.failure(parseError))
+				}
+			} else {
+				
+				let dataNilError = ResponseError(errorMessage: "The result data is nil", errorCode: 404)
+				completion(.failure(dataNilError))
+			}
+		})
 	}
 }
