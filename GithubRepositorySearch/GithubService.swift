afc3b022bff1ff996b5e30e585557f4a3d99529e
Muhammad Syah royni <syah.royni@dana.id>
2022-09-13 11:48:45 +0900

    Add GithubService to handle fetch api call for GithubFacade

---
+++ GithubRepositorySearch/GithubService.swift
@@ -0,0 +1,15 @@
+//
+//  GithubService.swift
+//  GithubRepositorySearch
+//
+//  Created by Muhammad Syah Royni on 13/09/22.
+//
+
+import Foundation
+
+class GithubService: GithubFacade {
+	
+	func searchRepository(queryString: String, completion: @escaping (Result<SearchRepositoryResult, Error>) -> Void) {
+		
+	}
+}
